0b44646a12aad5ecc18b8ac77fee3d11f806a96c
Racyn Ibaya <racynibaya@outlook.com>
2023-01-31 14:38:14 -0500

    connecting the frontend to deployed backend

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,13 +20,16 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:9000/api/v1/dalle', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          'https://dall-e-server-bvrb.onrender.com/api/v1/dalle',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
 
         const data = await response.json();
 
@@ -48,13 +51,16 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch(`http://localhost:9000/api/v1/post`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(form),
-        });
+        const response = await fetch(
+          `https://dall-e-server-bvrb.onrender.com/api/v1/post`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form),
+          }
+        );
 
         const data = await response.json();
         console.log(data);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,9 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:9000/api/v1/post');
+        const response = await fetch(
+          'https://dall-e-server-bvrb.onrender.com/api/v1/post'
+        );
 
         if (response.ok) {
           const result = await response.json();
